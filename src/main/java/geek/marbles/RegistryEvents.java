3decd61512e8f7066c69357555ba85ac62cc9b40
Robin Seifert <rseifert.phone@gmail.com>
2021-12-21 16:03:03 -0500

    Implemented basic item

---
+++ src/main/java/geek/marbles/RegistryEvents.java
@@ -0,0 +1,30 @@
+package geek.marbles;
+
+import geek.marbles.items.MarbleItem;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created by Robin Seifert on 12/21/2021.
+ */
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RegistryEvents
+{
+    @SubscribeEvent
+    public static void registerItems(final RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(
+                new MarbleItem(
+                        new Item.Properties()
+                                .stacksTo(16)
+                                .tab(CreativeModeTab.TAB_MISC)
+                ).setRegistryName("marble")
+        );
+    }
+}
--- src/main/java/geek/marbles/items/MarbleItem.java
+++ src/main/java/geek/marbles/items/MarbleItem.java
@@ -3,7 +3,6 @@ package geek.marbles.items;
 import net.minecraft.world.item.Item;
 
 public class MarbleItem extends Item {
-
   public MarbleItem(Item.Properties properties) {
     super(properties);
   }
