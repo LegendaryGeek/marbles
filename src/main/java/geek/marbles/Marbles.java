d41398a536f07805b5862ab9e2b51ae23af79175
Robin Seifert <rseifert.phone@gmail.com>
2021-12-21 17:57:18 -0500

    Small changes and set version to 1.18.1

--- src/main/java/geek/marbles/Marbles.java
+++ src/main/java/geek/marbles/Marbles.java
@@ -1,8 +1,12 @@
 package geek.marbles;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod("lgmarbles")
 public class Marbles
 {
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
 }
--- src/main/java/geek/marbles/RegistryEvents.java
+++ src/main/java/geek/marbles/RegistryEvents.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 /**
  * Created by Robin Seifert on 12/21/2021.
  */
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = "lgmarbles")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RegistryEvents
 {
