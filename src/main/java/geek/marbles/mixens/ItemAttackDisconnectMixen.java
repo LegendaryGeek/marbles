83580759dbae1aab07dbc2a3ec7c0407b342219e
Robin Seifert <rseifert.phone@gmail.com>
2021-12-22 17:00:22 -0500

    More mixen work

--- src/main/java/geek/marbles/mixens/ItemAttackDisconnectMixen.java
+++ src/main/java/geek/marbles/mixens/ItemAttackDisconnectMixen.java
@@ -1,7 +1,6 @@
 package geek.marbles.mixens;
 
 import geek.marbles.entity.MarbleEntity;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,14 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * Created by Robin Seifert on 12/22/2021.
  */
-@Mixin(ServerGamePacketListenerImpl.class)
+@Mixin(targets = "net.minecraft.server.network.ServerGamePacketListenerImpl$1")
 public class ItemAttackDisconnectMixen
 {
     @Shadow
     private Entity entity;
 
-    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl$1;onAttack()V", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "onAttack()V", cancellable = true)
     private void lgmarbles_onAttack2(CallbackInfo ci) {
+        System.out.println("Attack");
         if(this.entity instanceof MarbleEntity) {
             ci.cancel();
         }
